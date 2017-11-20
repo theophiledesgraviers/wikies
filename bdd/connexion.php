7e2b627a0bb7d289d101c546a4f99095762b64b3
theophiledesgraviers <t_desgraviers@live.fr>
2017-11-20 17:19:01 +0100

    dynamic notes

--- bdd/connexion.php
+++ bdd/connexion.php
@@ -5,6 +5,8 @@ $connexion = new PDO('mysql:host=localhost;dbname=wikies;charset=utf8', 'root', 
 
 function getNotes(){
 	global $connexion;
-	return $connexion->query("SELECT * FROM note")->fetchAll();
+	return $connexion->query("SELECT * FROM note")->fetchAll(PDO::FETCH_ASSOC);
 
 }
+
+
