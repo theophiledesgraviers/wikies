eaed3528202752a3cc8efddf6caaa1e6b5e581c6
theophiledesgraviers <t_desgraviers@live.fr>
2017-11-20 17:05:05 +0100

    Ajout de la connexion a la bdd

--- bdd/connexion.php
+++ bdd/connexion.php
@@ -1,3 +1,10 @@
 <?php 
 
+$connexion = new PDO('mysql:host=localhost;dbname=wikies;charset=utf8', 'root', '');
 // TODO creer un objet PDF_open_ccitt(pdfdoc, filename, width, height, BitReverse, k, Blackls1)
+
+function getNotes(){
+	global $connexion;
+	return $connexion->query("SELECT * FROM note")->fetchAll();
+
+}
